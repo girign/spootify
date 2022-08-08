0eb6cae2a9257e554d2f324dbef6ac352a4cdb72
girign <giridhargnair93@gmail.com>
2022-08-08 23:47:36 +0530

    axios interceptors now use fallback page constant

--- src/app/axiosWithInterceptors.js
+++ src/app/axiosWithInterceptors.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
+import { FALLBACK_PAGE } from "../app/constants";
+
 axios.interceptors.request.use(function (config) {
   config.headers.Authorization = `Bearer ${Cookies.get("accessToken")}`;
   return config;
@@ -16,7 +18,7 @@ axios.interceptors.response.use(
     console.log(error);
     if (error.code === "ERR_BAD_REQUEST") {
       Cookies.remove("accessToken");
-      window.location = "/login";
+      window.location = FALLBACK_PAGE;
     }
     return Promise.reject(error);
   }
