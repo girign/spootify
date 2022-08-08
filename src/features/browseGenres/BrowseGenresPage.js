d0c35018fc1e81e00f1d2146982a5a88070bdc6e
girign <giridhargnair93@gmail.com>
2022-08-08 20:39:34 +0530

    Implemented genre filter

--- src/features/browseGenres/BrowseGenresPage.js
+++ src/features/browseGenres/BrowseGenresPage.js
@@ -1,15 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchGenres } from "../browseGenres/genresSlice";
 
-import { List, Page, PageContent, Heading } from "grommet";
+import { List, Page, PageContent, Heading, TextInput } from "grommet";
 import { selectGenres } from "./genresSlice";
 import useCookieAuth from "../../hooks/useCookieAuth";
+import { Search } from "grommet-icons";
 
 function BrowseGenresPage() {
   useCookieAuth();
   const genres = useSelector(selectGenres);
-  console.log("came here rendering browse genres", genres);
+  const [filteredGenres, setFilteredGenres] = useState(genres);
   const dispatch = useDispatch();
   useEffect(() => {
     if (!genres.length) {
@@ -17,11 +18,27 @@ function BrowseGenresPage() {
     }
   }, []);
 
+  function filterGenres(term) {
+    if (!term) {
+      setFilteredGenres(genres);
+    } else {
+      let filtered = genres.filter((genre) =>
+        genre.includes(term.toLowerCase())
+      );
+      setFilteredGenres(filtered);
+    }
+  }
+
   return (
     <Page kind="narrow">
       <Heading>Browse Genres</Heading>
-      <PageContent kind="narrow">
-        <List data={genres} />
+      <TextInput
+        onChange={(e) => filterGenres(e.target.value)}
+        placeholder="Search for genre"
+        icon={<Search />}
+      />
+      <PageContent margin={{ top: "30px" }} kind="narrow" overflow="scroll">
+        <List data={filteredGenres} />
       </PageContent>
     </Page>
   );
