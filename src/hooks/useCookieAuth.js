c64dca2c087e4ccf3c9200e742f8d1d700e6668a
girign <giridhargnair93@gmail.com>
2022-08-08 23:45:32 +0530

    cookie hook now uses fallback page constant

--- src/hooks/useCookieAuth.js
+++ src/hooks/useCookieAuth.js
@@ -2,13 +2,15 @@ import Cookies from "js-cookie";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { FALLBACK_PAGE } from "../app/constants";
+
 function useCookieAuth() {
   const navigate = useNavigate();
 
   useEffect(() => {
     let accessToken = Cookies.get("accessToken");
     if (!accessToken) {
-      navigate("/login");
+      navigate(FALLBACK_PAGE);
     }
   }, []);
 }
